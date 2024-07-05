26bb125f6ebfb35a30219071210c0cb8d56e9775
sureshsubedii <sureshsubedi485@gmail.com>
2024-07-05 13:09:23 +0545

    alternative for into_decima

--- into_decimal.js
+++ into_decimal.js
@@ -14,3 +14,17 @@ var getDecimalValue = function(head) {
         return decimal
     
 };
+
+//In O(1) space complexity
+
+// var getDecimalValue = function(head) {
+//     let result = 0;
+//     let current = head;
+    
+//     while (current !== null) {
+//         result = (result << 1) | current.val;
+//         current = current.next;
+//     }
+    
+//     return result;
+// };
