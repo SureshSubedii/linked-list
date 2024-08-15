ca14c4cda1e240e741adf52c9d01a0d39c41c770
sureshsubedii <sureshsubedi485@gmail.com>
2024-08-15 10:33:27 +0545

    Spiral Matrix II revision

--- spiral_matrix_II.js
+++ spiral_matrix_II.js
@@ -1,26 +1,20 @@
-var generateMatrix = function(n) {
-    let matrix = Array.from({ length: n }, () => new Array(n));
-    let alreadyTraversed = []
-    
-    
-        let row = 0
-        let col = 0
-        let dirIndex = 0
-        let directions = [[0,1], [1,0], [0,-1], [-1,0]]
-        for(let i = 1; i <= n*n; i++){
-            alreadyTraversed.push([row,col])
-            matrix[row][col] = i
-    
-            let nextRow = row + directions[dirIndex][0]
-            let nextCol = col + directions[dirIndex][1]
-            let repeated = alreadyTraversed.some((arr) => arr.every((val,index)=> val === [nextRow,nextCol][index]))
-            if( nextRow >= n || nextCol >= n || nextCol < 0 || nextRow < 0 || repeated ){
-                dirIndex = (dirIndex + 1) % 4
-                nextRow = row + directions[dirIndex][0]
-                nextCol = col + directions[dirIndex][1]
-            }
-            row = nextRow
-            col = nextCol
+const generateMatrix = (n) =>{
+    let directions= [[0,1],[1,0], [0,-1], [-1,0]]
+    let dirIndex = 0
+    let result = new Array(n).fill(-1).map(()=> new Array(n).fill(-1))
+    let row = 0
+    let col = 0
+    for(let i = 1; i <= n * n ; i ++){
+        result[row][col] = i
+        let nextRow = row + directions[dirIndex][0]
+        let nextCol = col + directions[dirIndex][1]
+        if( nextCol >= n || nextRow >= n || nextCol < 0 || nextRow < 0 || result[nextRow]?.[nextCol] !== -1){
+            dirIndex = (1 + dirIndex) % 4
+            nextRow =  row + directions[dirIndex][0]
+            nextCol = col + directions[dirIndex][1]
         }
-        return matrix
-    };
+        row = nextRow
+        col = nextCol
+    }
+    return result
+}
